ac2e39de680b284dd12366d2f3b850c4a860c9b9
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-30 03:38:46 -0300

    Add: Players's name and life attribution in class Jogador, name validation using if and try_catch.

--- atividades/atividade_2.java
+++ atividades/atividade_2.java
@@ -5,31 +5,52 @@ import java.util.Scanner;
 public class atividade_2 {
     public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
+        double vidaJ1 = 100;
+        double vidaJ2 = 150;
+        String nome;
 
         class Jogador {
-            int vida;
-            String nome;
+            private double vida;
+            private String nome;
 
-            void printar_nome(String arg, int arg1){
+            public Jogador(String arg_nome, double arg_vida){
+                try{ 
+                    if (arg_nome == null || arg_nome.isBlank())
+                        throw new IllegalArgumentException();
+                    
+                    this.nome = arg_nome;
+                    this.vida = arg_vida;
+                    
+                } catch (Exception IllegalArgumentException) {
+                    System.out.println("Digite um nome válido!");
+                }
+
+                
+            }
+            
+            public void printar_nome(String arg, double arg1){
                 System.out.println("Nome do jogador: "+nome);
                 System.out.println("Vida do jogador: "+vida);
             }
 
         }
+        while(true){
+            System.out.print("Digite o nome do jogador 1: ");
+            nome = sc1.nextLine();
+            Jogador j1 = new Jogador(nome, vidaJ1);
 
-        Jogador j1 = new Jogador();
-        Jogador j2 = new Jogador();
+            System.out.print("Digite o nome do jogador 2: ");
+            nome = sc1.nextLine();
+            Jogador j2 = new Jogador(nome, vidaJ2);
 
-        j1.vida = 100;
-        System.out.print("Digite o nome do jogador 1: ");
-        j1.nome = sc1.nextLine();
+            if (j2.nome == null || j2.nome.isBlank() || j1.nome == null || j1.nome.isBlank())
+                continue;
 
-        System.out.print("Digite o nome do jogador 2: ");
-        j2.nome = sc1.nextLine();
-        j2.vida = 150;
+            j1.printar_nome(j1.nome, j1.vida);
+            j2.printar_nome(j1.nome, j1.vida);
 
-        j1.printar_nome(j1.nome, j1.vida);
-        j2.printar_nome(j1.nome, j1.vida);
+            break;
+        }
 
    } 
 }
